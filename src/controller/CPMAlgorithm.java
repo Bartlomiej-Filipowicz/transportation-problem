b3d465d3703b9e21b0653baf2ba8d2d3548f8428
Bart <carlo.filipowicz97@gmail.com>
2023-05-19 00:17:35 +0200

    working version

--- src/controller/CPMAlgorithm.java
+++ src/controller/CPMAlgorithm.java
@@ -4,6 +4,9 @@ import model.Task;
 
 import java.util.*;
 
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toCollection;
+
 public class CPMAlgorithm {
     private int numTasks;
     private List<Task> tasks;
@@ -16,7 +19,197 @@ public class CPMAlgorithm {
 
     private ArrayList<Integer> popyt;
     private ArrayList<Integer> podaz;
-    private ArrayList<ArrayList<Integer>> costs;
+    private ArrayList<ArrayList<Integer>> mycosts;
+
+    //new below
+    private static int[] demand;
+    private static int[] supply;
+    private static double[][] costs;
+    private static Shipment[][] matrix;
+
+    private static class Shipment {
+        final double costPerUnit;
+        final int r, c;
+        double quantity;
+
+        public Shipment(double q, double cpu, int r, int c) {
+            quantity = q;
+            costPerUnit = cpu;
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    static void init(ArrayList<Integer> podaz, ArrayList<Integer> popyt, ArrayList<ArrayList<Integer>> mcosts) {
+
+
+            supply = podaz.stream().mapToInt(i -> i).toArray();
+            demand = popyt.stream().mapToInt(i -> i).toArray();
+
+            costs = new double[supply.length][demand.length];
+            int[][] intcosts = mcosts.stream().map(  u  ->  u.stream().mapToInt(i->i).toArray()  ).toArray(int[][]::new);
+            for(int i = 0; i < intcosts.length; i++)
+            {
+                for(int j = 0; j < intcosts[0].length; j++)
+                    costs[i][j] = (double) intcosts[i][j];
+            }
+
+            matrix = new Shipment[supply.length][demand.length];
+
+    }
+
+    static void northWestCornerRule() {
+
+        for (int r = 0, northwest = 0; r < supply.length; r++)
+            for (int c = northwest; c < demand.length; c++) {
+
+                int quantity = Math.min(supply[r], demand[c]);
+                if (quantity > 0) {
+                    matrix[r][c] = new Shipment(quantity, costs[r][c], r, c);
+
+                    supply[r] -= quantity;
+                    demand[c] -= quantity;
+
+                    if (supply[r] == 0) {
+                        northwest = c;
+                        break;
+                    }
+                }
+            }
+    }
+
+    static void steppingStone() {
+        double maxReduction = 0;
+        Shipment[] move = null;
+        Shipment leaving = null;
+
+        fixDegenerateCase();
+
+        for (int r = 0; r < supply.length; r++) {
+            for (int c = 0; c < demand.length; c++) {
+
+                if (matrix[r][c] != null)
+                    continue;
+
+                Shipment trial = new Shipment(0, costs[r][c], r, c);
+                Shipment[] path = getClosedPath(trial);
+
+                double reduction = 0;
+                double lowestQuantity = Integer.MAX_VALUE;
+                Shipment leavingCandidate = null;
+
+                boolean plus = true;
+                for (Shipment s : path) {
+                    if (plus) {
+                        reduction += s.costPerUnit;
+                    } else {
+                        reduction -= s.costPerUnit;
+                        if (s.quantity < lowestQuantity) {
+                            leavingCandidate = s;
+                            lowestQuantity = s.quantity;
+                        }
+                    }
+                    plus = !plus;
+                }
+                if (reduction < maxReduction) {
+                    move = path;
+                    leaving = leavingCandidate;
+                    maxReduction = reduction;
+                }
+            }
+        }
+
+        if (move != null) {
+            double q = leaving.quantity;
+            boolean plus = true;
+            for (Shipment s : move) {
+                s.quantity += plus ? q : -q;
+                matrix[s.r][s.c] = s.quantity == 0 ? null : s;
+                plus = !plus;
+            }
+            steppingStone();
+        }
+    }
+
+    static LinkedList<Shipment> matrixToList() {
+        return stream(matrix)
+                .flatMap(row -> stream(row))
+                .filter(s -> s != null)
+                .collect(toCollection(LinkedList::new));
+    }
+
+    static Shipment[] getClosedPath(Shipment s) {
+        LinkedList<Shipment> path = matrixToList();
+        path.addFirst(s);
+
+        // remove (and keep removing) elements that do not have a
+        // vertical AND horizontal neighbor
+        while (path.removeIf(e -> {
+            Shipment[] nbrs = getNeighbors(e, path);
+            return nbrs[0] == null || nbrs[1] == null;
+        }));
+
+        // place the remaining elements in the correct plus-minus order
+        Shipment[] stones = path.toArray(new Shipment[path.size()]);
+        Shipment prev = s;
+        for (int i = 0; i < stones.length; i++) {
+            stones[i] = prev;
+            prev = getNeighbors(prev, path)[i % 2];
+        }
+        return stones;
+    }
+
+    static Shipment[] getNeighbors(Shipment s, LinkedList<Shipment> lst) {
+        Shipment[] nbrs = new Shipment[2];
+        for (Shipment o : lst) {
+            if (o != s) {
+                if (o.r == s.r && nbrs[0] == null)
+                    nbrs[0] = o;
+                else if (o.c == s.c && nbrs[1] == null)
+                    nbrs[1] = o;
+                if (nbrs[0] != null && nbrs[1] != null)
+                    break;
+            }
+        }
+        return nbrs;
+    }
+
+    static void fixDegenerateCase() {
+        final double eps = Double.MIN_VALUE;
+
+        if (supply.length + demand.length - 1 != matrixToList().size()) {
+
+            for (int r = 0; r < supply.length; r++)
+                for (int c = 0; c < demand.length; c++) {
+                    if (matrix[r][c] == null) {
+                        Shipment dummy = new Shipment(eps, costs[r][c], r, c);
+                        if (getClosedPath(dummy).length == 0) {
+                            matrix[r][c] = dummy;
+                            return;
+                        }
+                    }
+                }
+        }
+    }
+
+    static void printResult(String filename) {
+        System.out.printf("Optimal solution %s%n%n", filename);
+        double totalCosts = 0;
+
+        for (int r = 0; r < supply.length; r++) {
+            for (int c = 0; c < demand.length; c++) {
+
+                Shipment s = matrix[r][c];
+                if (s != null && s.r == r && s.c == c) {
+                    System.out.printf(" %3s ", (int) s.quantity);
+                    totalCosts += (s.quantity * s.costPerUnit);
+                } else
+                    System.out.printf("  -  ");
+            }
+            System.out.println();
+        }
+        System.out.printf("%nTotal costs: %s%n%n", totalCosts);
+    }
 
     public CPMAlgorithm() {
         /*
@@ -32,7 +225,8 @@ public class CPMAlgorithm {
     public CPMAlgorithm update(ArrayList<Integer> podaz, ArrayList<Integer> popyt, ArrayList<ArrayList<Integer>> costs) {
         this.podaz = podaz;
         this.popyt = popyt;
-        this.costs = costs;
+        this.mycosts = costs;
+        init(podaz,popyt,costs);
         /*
         this.numTasks = numTasks;
         this.tasks = tasks; // LISTA zadan
@@ -58,6 +252,10 @@ public class CPMAlgorithm {
         ArrayList<Integer> v = new ArrayList<Integer>(); // v -> w poziomie
         ArrayList<ArrayList<Integer>> delta = new ArrayList<ArrayList<Integer>>();
 
+        northWestCornerRule();
+        steppingStone();
+        steppingStone(); // !!!!!! experiment with number of calling steppingStone()
+        printResult("myfile.txt");
 
         for(int i = 0; i < rowNum; i++){
             northWestTable.add(new ArrayList<Integer>());
@@ -88,29 +286,29 @@ public class CPMAlgorithm {
                 }
             }
         }
-        System.out.println(northWestTable);
+        //System.out.println(northWestTable);
         u.add(0);
         for(int i = 0; i < rowNum; i++){
             for(int j = 0; j < colNum; j++)
                 if(onPath.get(i).get(j)){
                     if(u.size() == i + 1){
-                        v.add(costs.get(i).get(j) - u.get(i));
+                        v.add(mycosts.get(i).get(j) - u.get(i));
                     }
                     else{
-                        u.add(costs.get(i).get(j) - v.get(j));
+                        u.add(mycosts.get(i).get(j) - v.get(j));
                     }
                 }
         }
-        System.out.println(u);
-        System.out.println(v);
+//        System.out.println(u);
+//        System.out.println(v);
 
         for(int i = 0; i < rowNum; i++){
             for(int j = 0; j < colNum; j++)
                 if(!onPath.get(i).get(j)){
-                    delta.get(i).set(j, u.get(i) + v.get(j) - costs.get(i).get(j));
+                    delta.get(i).set(j, u.get(i) + v.get(j) - mycosts.get(i).get(j));
                 }
         }
-        System.out.println(delta);
+       // System.out.println(delta);
         int maks_delta = Integer.MIN_VALUE;
         int pos_i = 0, pos_j = 0;
         for(int i = 0; i < rowNum; i++){
@@ -122,8 +320,8 @@ public class CPMAlgorithm {
                 }
             }
         }
-        System.out.println(maks_delta);
-        System.out.println(pos_i + "  " + pos_j);
+//        System.out.println(maks_delta);
+//        System.out.println(pos_i + "  " + pos_j);
         if(maks_delta <= 0){
             // the optimal solution found
             return;
@@ -142,17 +340,22 @@ public class CPMAlgorithm {
             int i_loc = start_up, j_loc = pos_j; // iterators
             int min_even = Integer.MAX_VALUE;
             while (true){
+
                 counter++;
                 if(counter%2 == 0){
                     min_even = Math.min(min_even, northWestTable.get(i_loc).get(j_loc));
                 }
                 if(onPath.get(i_loc + 1).get(j_loc)) i_loc += 1;
                 else j_loc += 1;
+                break; // <-- delete
             }
         }
         else{
             //code
         }
+//        northWestCornerRule();
+//        steppingStone();
+//        printResult("myfile.txt");
         /*
         // Step 0: check if there are nodes/activities with 0 next
         if (!tasks.get(tasks.size()-1).getName().equals("")) {
